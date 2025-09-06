6c610ef81de1cb0bbb7ab6988972a5e79891aa7c
Lekha G <kgdivyalekha@gmail.com>
2025-09-06 14:43:47 +0530

    Committing DSA Code

--- untitled/src/com/java/dsa/RemoveDuplicatesSortedList.java
+++ untitled/src/com/java/dsa/RemoveDuplicatesSortedList.java
@@ -1,5 +1,6 @@
 package com.java.dsa;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -13,16 +14,40 @@ class ListNode {
 
 public class RemoveDuplicatesSortedList {
     public static void main(String[] args) {
-        List<ListNode> list = new LinkedList<>();
-        list.add(new ListNode(1));
-        list.add(new ListNode(1));
-        list.add(new ListNode(2));
-        list.add(new ListNode(3));
-        list.add(new ListNode(3));
-        list.stream().forEach(l->System.out.println(l.val));
-      //  deleteDuplicates(list.get(0));
+        ListNode head = new ListNode(1);
+        ListNode node=new ListNode(2);
+        head.next=node;
+        ListNode node2=new ListNode(2);
+        node.next=node2;
+        ListNode node3=new ListNode(3);
+        node2.next=node3;
+        ListNode node4=new ListNode(3);
+        node3.next=node4;
+        node4.next=null;
+        head=deleteDuplicates(head);
+        while(head!=null)
+        {
+            System.out.print(head.val+ "==>");
+            head=head.next;
+        }
+        System.out.println("null");
     }
-    public ListNode deleteDuplicates(ListNode head) {
-    return null;
+    public static ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        ListNode prev=null;
+        HashSet<Integer> set=new HashSet<>();
+        while(node!=null)
+        {
+            if(set.contains(node.val))
+            {
+                prev.next=node.next;
+            }
+            else {
+                set.add(node.val);
+                prev=node;
+            }
+            node=node.next;
+        }
+        return head;
     }
 }
