5a45df73fc9ab5600144184a0356ec3ff41736b5
kgdivyalekha <kgdivyalekha@gmail.com>
2025-10-07 16:37:12 -0400

    Committing DSA

---
+++ untitled/src/com/java/dsa/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package com.java.dsa;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbingStairs(2));
+    }
+
+    private static int climbingStairs(int stairs) {
+        int[] dp=new int[stairs+1];
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=stairs;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[stairs];
+    }
+}
